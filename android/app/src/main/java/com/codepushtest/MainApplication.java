97967b850a1aa9c2aa3e44bd9c42850ad202cc89
Thomas Morris <tmorris@salsitasoft.com>
2018-03-22 14:15:06 +0100

    Initial commit - works with iOS - must release to see changes in dev

---
+++ android/app/src/main/java/com/codepushtest/MainApplication.java
@@ -0,0 +1,54 @@
+package com.codepushtest;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            // TODO: put in the same key as the ios one and try
+            new CodePush("OB6TYqvZSzLQWHW4LNYhy63xqRLO34e4c2f7-8101-45fc-b5ea-fe2ae8ce628a", getApplicationContext(), BuildConfig.DEBUG)
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
